5d89aacb30c07f1d600e7acfaef06b83eb95b2f6
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-02-28 16:07:39 +0700

    complete unsolved exercise

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,28 @@
+"""
+    Prime factors exercise
+"""
+
+from math import sqrt
+
+def is_prime(value):
+    """
+        Check if a number is a prime number
+    """
+    for i in range(2, int(sqrt(value)) + 1):
+        if value % i == 0:
+            return False
+    return True
+
 def factors(value):
-    pass
+    """
+        Get all prime factors of a number
+    """
+    result = []
+    prime = 2
+    while value != 1:
+        if value % prime == 0 and is_prime(prime):
+            result.append(prime)
+            value //= prime
+        else:
+            prime += 1
+    return result
